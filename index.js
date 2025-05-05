2ab729e5442f9da2114986c42a6d53f64681379c
eddyseed <randomboiii069@gmail.com>
2025-05-05 21:31:47 +0530

    chore: update package.json to use nodemon for development and add nodemon as a dependency; set default PORT in sample.env

--- index.js
+++ index.js
@@ -2,23 +2,48 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const dns = require('dns');
+const bodyParser = require('body-parser');
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+let urlDatabase = {};
+let currentId = 1;
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function (req, res) {
+  const longUrl = req.body.url;
+  const hostname = longUrl.replace(/^https?:\/\//, '').split('/')[0];
+  dns.lookup(hostname, (err, address) => {
+    if (err) {
+      return res.json({ error: 'invalid url' });
+    } else {
+      urlDatabase[currentId] = longUrl;
+      currentId++;
+      return res.json({
+        original_url: longUrl,
+        short_url: currentId - 1
+      });
+    }
+  });
 });
-
-app.listen(port, function() {
+app.get('/api/shorturl/:urlID', (req, res) => {
+  const id = req.params.urlID
+  const longUrl = urlDatabase[id];
+  if (longUrl) {
+    res.redirect(longUrl);
+  }
+  else {
+    res.json({ error: 'invalid url' });
+  }
+})
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
